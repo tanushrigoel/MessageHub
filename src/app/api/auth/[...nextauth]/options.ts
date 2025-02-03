d6c447dea76e560a74a00b92db6f4f069db36aa4
Tanushri goel <tanushri@lpu.in>
2025-02-03 20:48:33 +0530

    fix: update secure cookie setting to use NODE_ENV directly

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -147,7 +147,7 @@ export const authOptions: NextAuthOptions = {
       name: "next-auth.session-token",
       options: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV,
         path: "/",
         sameSite: "lax",
         maxAge: 7 * 24 * 60 * 60,
